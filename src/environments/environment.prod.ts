292e93183da70eff6a762533b3a3eaab023952b1
Anthony Juarez Solis <anthonyjuarezsolis@gmail.com>
2022-08-08 00:57:05 -0500

    add script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAlaoHxwJHpZ_uxvA-WRQo4UiS1wIX88bk",
+    authDomain: "bakanazo-web-app.firebaseapp.com",
+    projectId: "bakanazo-web-app",
+    storageBucket: "bakanazo-web-app.appspot.com",
+    messagingSenderId: "760131181",
+    appId: "1:760131181:web:06b638a8130b5677e4d964",
+    measurementId: "G-PRGQR95V5D",
+  },
 };
